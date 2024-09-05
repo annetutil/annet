d1f8376f1f1d97dd716e9613b57d56b4e3b382e3
Vlad Starostin <vladstar@yandex-team.ru>
2024-09-05 12:53:25 +0300

    NOCDEV-14656: option for create output dir even for blackboxes

--- annet/cli_args.py
+++ annet/cli_args.py
@@ -70,6 +70,16 @@ opt_expand_path = Arg(
     help="Use full paths of Entire-generators and no just names when writing them to the file system"
 )
 
+opt_dest_force_create_dir = Arg(
+    "--dest-force-create-dir", default=False,
+    help="Output generated data to dir, even for blackboxes"
+)
+
+opt_blackbox_config_filename = Arg(
+    "--blackbox-config-filename", default="config.cfg",
+    help="Filename for blackbox config (applicable only with --dest-force-create-dir)"
+)
+
 opt_old = Arg(
     "old",
     help="A path to a file (or a directory with a batch of files) that contains the old config"
@@ -426,6 +436,8 @@ class FileOutOptions(ArgGroup):
     expand_path = opt_expand_path
     no_label = opt_no_label
     no_color = opt_no_color
+    dest_force_create_dir = opt_dest_force_create_dir
+    blackbox_config_filename = opt_blackbox_config_filename
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
