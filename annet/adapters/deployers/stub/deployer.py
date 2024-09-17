1efe5436a01b3a135e4b28fd7aa5de0843aa5dd4
Aleksandr Balezin <gescheit@yandex-team.ru>
2024-09-17 18:41:07 +0300

    fix exception

--- annet/adapters/deployers/stub/deployer.py
+++ annet/adapters/deployers/stub/deployer.py
@@ -4,13 +4,13 @@ from annet.annlib.netdev.views.hardware import HardwareView
 
 class StubDeployDriver(DeployDriver):
     async def bulk_deploy(self, deploy_cmds: dict, args: DeployOptions) -> DeployResult:
-        NotImplementedError()
+        raise NotImplementedError()
 
     def apply_deploy_rulebook(self, hw: HardwareView, cmd_paths, do_finalize=True, do_commit=True):
-        NotImplementedError()
+        raise NotImplementedError()
 
     def build_configuration_cmdlist(self, hw: HardwareView, do_finalize=True, do_commit=True):
-        NotImplementedError()
+        raise NotImplementedError()
 
     def build_exit_cmdlist(self, hw):
         raise NotImplementedError()
