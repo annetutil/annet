656790ff6acb62298820845d38002a8e074f5d88
Aleksandr Balezin <gescheit@yandex-team.ru>
2024-09-17 16:08:50 +0300

    fix

--- annet/lib.py
+++ annet/lib.py
@@ -4,7 +4,7 @@ import shutil
 import sys
 from functools import lru_cache
 from pathlib import Path
-from typing import Awaitable, Optional, Dict, Any
+from typing import Awaitable, Optional
 
 import yaml
 from annet.annlib.lib import (  # pylint: disable=unused-import
