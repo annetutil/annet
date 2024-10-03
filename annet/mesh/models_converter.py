3c39ef342b70a5252e1e3f15bd010b1b21d7600c
Andrey Tikhonov <17@itishka.org>
2024-10-03 17:15:39 +0200

    convert peer options

--- annet/mesh/models_converter.py
+++ annet/mesh/models_converter.py
@@ -2,9 +2,8 @@ from ipaddress import ip_interface
 
 from adaptix import Retort, loader, Chain, name_mapping
 
-from .basemodel import Special
 from .peer_models import PeerDTO
-from ..bgp_models import GlobalOptions, VrfOptions, FamilyOptions, Peer, PeerGroup, ASN
+from ..bgp_models import GlobalOptions, VrfOptions, FamilyOptions, Peer, PeerGroup, ASN, PeerOptions
 from ..storage import Device
 
 
@@ -28,37 +27,35 @@ retort = Retort(
         loader(GlobalOptions, ObjMapping, Chain.FIRST),
         loader(VrfOptions, ObjMapping, Chain.FIRST),
         loader(FamilyOptions, ObjMapping, Chain.FIRST),
+        loader(PeerOptions, ObjMapping, Chain.FIRST),
+        name_mapping(PeerOptions, map={
+            "local_as": "asnum",
+        })
     ]
 )
 
 to_bgp_global_options = retort.get_loader(GlobalOptions)
 
 
-def drop_unset(data: dict):
-    return {
-        k: v
-        for k, v in data.items()
-        if v is not Special.NOT_SET
-    }
-
-
-
 def to_bgp_peer(local: PeerDTO, connected: PeerDTO, connected_device: Device) -> Peer:
+    options = retort.load(local, PeerOptions)
     result = Peer(
         addr=str(ip_interface(connected.addr).ip),
         remote_as=ASN(connected.asnum),
         name=connected.name,
-        families = connected.families,
+        families=connected.families,
+        hostname=connected_device.hostname,
+        options=options,
     )
-    #connected
+    # connected
     result.vrf_name = getattr(connected, "vrf", result.vrf_name)
     result.description = getattr(connected, "description", result.description)
-    #local
+    # local
     result.import_policy = getattr(connected, "import_policy", result.import_policy)
     result.export_policy = getattr(connected, "export_policy", result.export_policy)
 
     if hasattr(local, "group"):
-        result.group=PeerGroup(
+        result.group = PeerGroup(
             name=local.group.name,
             internal_name="",
             update_source="",
