cf237131abccadab3fedd7946100b12f643e5ce1
Andrey Tikhonov <17@itishka.org>
2024-10-10 16:41:37 +0200

    peer options are optional

--- annet/bgp_models.py
+++ annet/bgp_models.py
@@ -74,52 +74,53 @@ Family = Literal["ipv4_unicast", "ipv6_unicast", "ipv4_labeled", "ipv6_labeled"]
 
 @dataclass(frozen=True, slots=True, kw_only=True)
 class PeerOptions:
-    local_as: ASN = ASN(None)
-    unnumbered: bool = False
-    rr_client: bool = False
-    next_hop_self: bool = False
-    extended_next_hop: bool = False
-    send_community: bool = False
-    send_lcommunity: bool = False
-    send_extcommunity: bool = False
-    send_labeled: bool = False
-    import_limit: bool = False
-    teardown_timeout: bool = False
-    redistribute: bool = False
-    passive: bool = False
-    mtu_discovery: bool = False
-    advertise_inactive: bool = False
-    advertise_bgp_static: bool = False
-    allowas_in: bool = False
-    auth_key: bool = False
-    add_path: bool = False
-    multipath: bool = False
-    multihop: bool = False
-    multihop_no_nexthop_change: bool = False
-    af_no_install: bool = False
-    bfd: bool = False
-    rib: bool = False
+    """The same options as for group but any field is optional"""
+    local_as: ASN | None = None
+    unnumbered: bool | None = None
+    rr_client: bool | None = None
+    next_hop_self: bool | None = None
+    extended_next_hop: bool | None = None
+    send_community: bool | None = None
+    send_lcommunity: bool | None = None
+    send_extcommunity: bool | None = None
+    send_labeled: bool | None = None
+    import_limit: bool | None = None
+    teardown_timeout: bool | None = None
+    redistribute: bool | None = None
+    passive: bool | None = None
+    mtu_discovery: bool | None = None
+    advertise_inactive: bool | None = None
+    advertise_bgp_static: bool | None = None
+    allowas_in: bool | None = None
+    auth_key: bool | None = None
+    add_path: bool | None = None
+    multipath: bool | None = None
+    multihop: bool | None = None
+    multihop_no_nexthop_change: bool | None = None
+    af_no_install: bool | None = None
+    bfd: bool | None = None
+    rib: bool | None = None
     bfd_timers: BFDTimers | None = None
-    resolve_vpn: bool = False
+    resolve_vpn: bool | None = None
     af_rib_group: str | None = None
-    af_loops: int = 0
-    hold_time: int = 0
-    listen_network: bool = False
-    remove_private: bool = False
-    as_override: bool = False
-    aigp: bool = False
-    bmp_monitor: bool = False
-    no_prepend: bool = False
-    no_explicit_null: bool = False
-    uniq_iface: bool = False
-    advertise_peer_as: bool = False
-    connect_retry: bool = False
-    advertise_external: bool = False
-    advertise_irb: bool = False
-    listen_only: bool = False
-    soft_reconfiguration_inbound: bool = False
-    not_active: bool = False
-    mtu: int = 0
+    af_loops: int | None = None
+    hold_time: int | None = None
+    listen_network: bool | None = None
+    remove_private: bool | None = None
+    as_override: bool | None = None
+    aigp: bool | None = None
+    bmp_monitor: bool | None = None
+    no_prepend: bool | None = None
+    no_explicit_null: bool | None = None
+    uniq_iface: bool | None = None
+    advertise_peer_as: bool | None = None
+    connect_retry: bool | None = None
+    advertise_external: bool | None = None
+    advertise_irb: bool | None = None
+    listen_only: bool | None = None
+    soft_reconfiguration_inbound: bool | None = None
+    not_active: bool | None = None
+    mtu: int | None = None
 
 
 @dataclass(slots=True, kw_only=True)
@@ -178,12 +179,60 @@ class FamilyOptions:
 
 
 @dataclass(frozen=True, slots=True, kw_only=True)
-class PeerGroup(PeerOptions):
+class PeerGroup:
     name: str
     remote_as: ASN = ASN(None)
     internal_name: str = ""
     description: str | None = None
     update_source: str | None = None
+
+    # more strict version of PeerOptions
+    local_as: ASN = ASN(None)
+    unnumbered: bool = False
+    rr_client: bool = False
+    next_hop_self: bool = False
+    extended_next_hop: bool = False
+    send_community: bool = False
+    send_lcommunity: bool = False
+    send_extcommunity: bool = False
+    send_labeled: bool = False
+    import_limit: bool = False
+    teardown_timeout: bool = False
+    redistribute: bool = False
+    passive: bool = False
+    mtu_discovery: bool = False
+    advertise_inactive: bool = False
+    advertise_bgp_static: bool = False
+    allowas_in: bool = False
+    auth_key: bool = False
+    add_path: bool = False
+    multipath: bool = False
+    multihop: bool = False
+    multihop_no_nexthop_change: bool = False
+    af_no_install: bool = False
+    bfd: bool = False
+    rib: bool = False
+    bfd_timers: BFDTimers | None = None
+    resolve_vpn: bool = False
+    af_rib_group: str | None = None
+    af_loops: int = 0
+    hold_time: int = 0
+    listen_network: bool = False
+    remove_private: bool = False
+    as_override: bool = False
+    aigp: bool = False
+    bmp_monitor: bool = False
+    no_prepend: bool = False
+    no_explicit_null: bool = False
+    uniq_iface: bool = False
+    advertise_peer_as: bool = False
+    connect_retry: bool = False
+    advertise_external: bool = False
+    advertise_irb: bool = False
+    listen_only: bool = False
+    soft_reconfiguration_inbound: bool = False
+    not_active: bool = False
+    mtu: int = 0
 
 
 @dataclass(slots=True, kw_only=True)
