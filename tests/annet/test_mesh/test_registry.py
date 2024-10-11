3926617705cdd5134b9a2c3389c21c1a9bdb51ff
Andrey Tikhonov <17@itishka.org>
2024-10-11 14:42:07 +0200

    Test registry logic

--- tests/annet/test_mesh/test_registry.py
+++ tests/annet/test_mesh/test_registry.py
@@ -1,4 +1,6 @@
-from annet.mesh.match_args import Left
+import pytest
+
+from annet.mesh.match_args import Left, Right
 from annet.mesh.registry import MeshRulesRegistry
 
 
@@ -14,8 +16,9 @@ def baz(*args, **kwargs) -> None:
     pass
 
 
-def test_global():
-    registry = MeshRulesRegistry()
+@pytest.mark.parametrize("short", [True, False])
+def test_global(short):
+    registry = MeshRulesRegistry(short)
     registry.device("left1-{x}")(foo)
     registry.device("left1-{x}", Left.x > 1)(bar)
     registry.device("left2-{x}")(baz)
@@ -24,3 +27,61 @@ def test_global():
     assert len(found) == 1
     assert found[0].handler is foo
     assert found[0].matched.x == 1
+
+    short_found = registry.lookup_global("left1-1.example.com")
+    if short:
+        assert short_found == found
+    else:
+        assert short_found == []
+
+
+@pytest.mark.parametrize("short", [True, False])
+def test_direct(short):
+    registry = MeshRulesRegistry(short)
+    registry.direct("left1-{x}", "right1-{x}")(foo)
+    registry.direct("left1-{x}", "right1-{x}", Left.x > Right.x)(bar)
+    registry.direct("left2-{x}", "right1-{x}")(baz)
+
+    found = registry.lookup_direct(
+        "left1-1",
+        ["right1-2", "right2"],
+    )
+    assert len(found) == 1
+    assert found[0].handler is foo
+    assert found[0].matched_left.x == 1
+    assert found[0].matched_right.x == 2
+
+    short_found = registry.lookup_direct(
+        "left1-1.example.com",
+        ["right1-2.example.com", "right2-2.example.com"],
+    )
+    if short:
+        assert short_found == found
+    else:
+        assert short_found == []
+
+
+@pytest.mark.parametrize("short", [True, False])
+def test_indirect(short):
+    registry = MeshRulesRegistry(short)
+    registry.indirect("left1-{x}", "right1-{x}")(foo)
+    registry.indirect("left1-{x}", "right1-{x}", Left.x > Right.x)(bar)
+    registry.indirect("left2-{x}", "right1-{x}")(baz)
+
+    found = registry.lookup_indirect(
+        "left1-1",
+        ["right1-2", "right2"],
+    )
+    assert len(found) == 1
+    assert found[0].handler is foo
+    assert found[0].matched_left.x == 1
+    assert found[0].matched_right.x == 2
+
+    short_found = registry.lookup_indirect(
+        "left1-1.example.com",
+        ["right1-2.example.com", "right2-2.example.com"],
+    )
+    if short:
+        assert short_found == found
+    else:
+        assert short_found == []
