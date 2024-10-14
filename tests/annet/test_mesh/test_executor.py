b45e9d926afcc60f525758a7f98efd352f68d31c
Andrey Tikhonov <17@itishka.org>
2024-10-14 14:55:32 +0200

    test executor

--- tests/annet/test_mesh/test_executor.py
+++ tests/annet/test_mesh/test_executor.py
@@ -2,7 +2,7 @@ from pprint import pprint
 
 import pytest
 
-from annet.mesh import MeshExecutor, MeshRulesRegistry, GlobalOptions, DirectPeer, MeshSession
+from annet.mesh import MeshExecutor, MeshRulesRegistry, GlobalOptions, DirectPeer, MeshSession, IndirectPeer
 from .fakes import FakeStorage, FakeDevice, FakeInterface
 
 VRF = "testvrf"
@@ -24,16 +24,27 @@ def on_direct(local: DirectPeer, neighbor: DirectPeer, session: MeshSession):
     session.families = {"ipv4_unicast"}
 
 
+def on_indirect(local: IndirectPeer, neighbor: IndirectPeer, session: MeshSession):
+    local.addr = "192.168.1.254"
+    neighbor.addr = "192.168.1.10"
+    neighbor.name = "YYY"
+    local.mtu = 1505
+    neighbor.mtu = 1506
+    session.asnum = 12340
+    session.families = {"ipv6_unicast"}
+
+
 @pytest.fixture
 def registry():
     r = MeshRulesRegistry()
     r.device("{x:.*}")(on_device_x)
-    r.direct("dev{num}.example.com", "{x:.*}")(on_direct)
+    r.direct("dev{num}.example.com", "dev_{x:.*}")(on_direct)
+    r.indirect("dev{num}.example.com", "dev{num}.remote.example.com")(on_indirect)
     return r
 
 
 DEV1 = "dev1.example.com"
-DEV2 = "dev2.example.com"
+DEV2 = "dev2.remote.example.com"
 DEV_NEIGHBOR = "dev_neighbor.example.com"
 
 
@@ -80,4 +91,27 @@ def test_storage(registry, storage, device1):
     r = MeshExecutor(registry, storage)
     res = r.execute_for(device1)
     pprint(res)
-    assert False
+
+    assert res.global_options.groups == []
+    assert res.global_options.vrf.keys() == {VRF}
+    vrf = res.global_options.vrf[VRF]
+    assert vrf.vrf_name == VRF
+    assert vrf.static_label is None
+    assert len(vrf.groups) == 1
+    assert vrf.groups[0].mtu == 1499
+    assert vrf.groups[0].name == GROUP
+
+    peer_direct, peer_indirect = res.peers
+    assert peer_direct.name == "XXX"
+    assert peer_direct.addr == "192.168.1.1"
+    assert peer_direct.options.mtu == 1501
+    assert peer_direct.families == {"ipv4_unicast"}
+    assert peer_direct.remote_as == 12345
+    assert peer_direct.description == ""
+
+    assert peer_indirect.name == "YYY"
+    assert peer_indirect.addr == "192.168.1.10"
+    assert peer_indirect.options.mtu == 1505
+    assert peer_indirect.families == {"ipv6_unicast"}
+    assert peer_indirect.remote_as == 12340
+    assert peer_indirect.description == ""
