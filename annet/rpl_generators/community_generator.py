32d13995531f7a2f42bcfdb71f66574330b2f9b1
Andrey Tikhonov <17@itishka.org>
2024-11-26 14:07:37 +0100

    filter communities using RPL

--- annet/rpl_generators/community_generator.py
+++ annet/rpl_generators/community_generator.py
@@ -2,6 +2,7 @@ from collections.abc import Sequence
 from typing import Any
 
 from annet.generators import PartialGenerator
+from annet.rpl import RouteMap, SingleCondition
 from .entities import CommunityList, CommunityLogic, CommunityType
 
 
@@ -27,7 +28,37 @@ class CommunityListGenerator(PartialGenerator):
         """
 
     def get_community_lists(self, device: Any) -> list[CommunityList]:
+        # TODO filter using RPL
         return []
+
+    def get_used_community_lists(self, device: Any) -> list[CommunityList]:
+        communities = {c.name: c for c in self.get_community_lists(device)}
+        policies = self.get_routemap().apply(device)
+
+        used_communities: set[str] = set()
+        for policy in policies:
+            for statement in policy.statements:
+                condition: SingleCondition[Sequence[str]]
+                for condition in statement.match.find_all("community"):
+                    used_communities.update(condition.value)
+                for condition in statement.match.find_all("extcommunity"):
+                    used_communities.update(condition.value)
+                for action in statement.then.find_all("community"):
+                    if action.value.replaces is not None:
+                        used_communities.update(action.value.replaced)
+                    used_communities.update(action.value.added)
+                    used_communities.update(action.value.removed)
+                for action in statement.then.find_all("extcommunity"):
+                    if action.value.replaces is not None:
+                        used_communities.update(action.value.replaced)
+                    used_communities.update(action.value.added)
+                    used_communities.update(action.value.removed)
+        return [
+            communities[name] for name in used_communities
+        ]
+
+    def get_routemap(self) -> RouteMap:
+        return RouteMap()
 
     def _huawei_community_filter(self, index: int, community_list: CommunityList, members: str) -> Sequence[str]:
         if community_list.type is CommunityType.BASIC:
@@ -42,7 +73,7 @@ class CommunityListGenerator(PartialGenerator):
             raise NotImplementedError(f"CommunityList type {community_list.type} not implemented for huawei")
 
     def run_huawei(self, device: Any):
-        for community_list in self.get_community_lists(device):
+        for community_list in self.get_used_community_lists(device):
             if community_list.type is CommunityType.RT:
                 # RT communities used with prefix rt
                 members = [f"rt {m}" for m in community_list.members]
