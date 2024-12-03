4bdbaaa1e66e56f28b41bd97ca647073f76355ed
Andrey Tikhonov <17@itishka.org>
2024-12-03 10:18:32 +0100

    Speedup mesh using brief devices

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ if __name__ == "__main__":
           ],
         },
         extras_require={
-            "netbox": ["annetbox[sync]>=0.1.8"],
+            "netbox": ["annetbox[sync]>=0.1.10"],
         },
         python_requires=">=3.10",
         install_requires=requirements(),
