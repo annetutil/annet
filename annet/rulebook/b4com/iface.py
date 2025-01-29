1445ce770979aef11e567049f19df376934f9456
Roman Karaulanov <anteron@mws.ru>
2025-01-29 20:35:39 +0300

    Add sflow support + deploy command fix

--- annet/rulebook/b4com/iface.py
+++ annet/rulebook/b4com/iface.py
@@ -3,13 +3,28 @@ from annet.annlib.types import Op
 from annet.rulebook import common
 
 
-def mtu(old, new, diff_pre, **_):
+def mtu(rule, key, diff, **kwargs):
     """
-    Для того, чтобы удалить NTP из CFS, сначала нужно сбросить активные
-    NTP сессии.
+    Удаляем mtu без указания значения
     """
     if diff[Op.REMOVED]:
         yield (False, "no mtu", None)
     elif diff[Op.ADDED]:
-        yield from common.default(rule, key, diff)
+        yield from common.default(rule, key, diff, **kwargs)
 
+
+def sflow(rule, key, diff, **kwargs):
+    """
+    Команда sflow sampling-rate * direction ingress max-header-size *
+    сносится без указания sampling-rate и max-header-size
+    """
+    result = common.default(rule, key, diff, **kwargs)
+    for op, cmd, ch in result:
+        if diff[Op.REMOVED]:
+            if "ingress" in diff[Op.REMOVED][0]["row"]:
+                yield (op, "no sflow sampling-rate direction ingress", ch)
+            elif "egress" in diff[Op.REMOVED][0]["row"]:
+                yield (op, "no sflow sampling-rate direction egress", ch)
+            else:
+                yield (op, cmd, ch)
+    return result
