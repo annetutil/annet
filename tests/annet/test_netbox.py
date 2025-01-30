236aae569ee4a4430c76e496b2effb101d66073a
gescheit <gescheit12@gmail.com>
2025-01-30 21:22:45 +0300

    Create test_netbox.py

---
+++ tests/annet/test_netbox.py
@@ -0,0 +1,12 @@
+from annet.adapters.netbox.v37.storage import parse_glob
+import pytest
+
+
+def test_parse_glob():
+    assert parse_glob(["host"]) == {"name__ic": ["host"], "site": [], "tag": []}
+    assert parse_glob(["site:mysite"]) == {"name__ic": [], "site": ["mysite"], "tag": []}
+    assert parse_glob(["tag:mysite","justhost"]) == {"name__ic": ["justhost"], "site": [], "tag": ["mysite"]}
+    with pytest.raises(Exception):
+        parse_glob(["host:"])
+    with pytest.raises(Exception):
+        parse_glob(["NONONO:param"])
