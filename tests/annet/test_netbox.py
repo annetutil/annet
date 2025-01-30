0f7cfbca368968b18104dabbf3c11d45e7c3a6d7
gescheit <gescheit12@gmail.com>
2025-01-30 21:23:45 +0300

    Update test_netbox.py

--- tests/annet/test_netbox.py
+++ tests/annet/test_netbox.py
@@ -5,7 +5,7 @@ import pytest
 def test_parse_glob():
     assert parse_glob(["host"]) == {"name__ic": ["host"], "site": [], "tag": []}
     assert parse_glob(["site:mysite"]) == {"name__ic": [], "site": ["mysite"], "tag": []}
-    assert parse_glob(["tag:mysite","justhost"]) == {"name__ic": ["justhost"], "site": [], "tag": ["mysite"]}
+    assert parse_glob(["tag:mysite", "justhost"]) == {"name__ic": ["justhost"], "site": [], "tag": ["mysite"]}
     with pytest.raises(Exception):
         parse_glob(["host:"])
     with pytest.raises(Exception):
