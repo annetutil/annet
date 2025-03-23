dbbb76ecdf2d74422efb9ee3d4929d426e03850a
shurick2 <shurickunix@gmail.com>
2025-03-23 10:21:25 +0300

    Update provider.py

--- annet/adapters/netbox/provider.py
+++ annet/adapters/netbox/provider.py
@@ -37,7 +37,7 @@ def storage_factory(opts: NetboxStorageOpts) -> Storage:
             raise ValueError(f"Unsupported version: {status.netbox_version}")
     except ClientLibraryError:
         raise ValueError(f"Connection error: Unable to reach Netbox at URL: {opts.url}")
-    raise Exception(f"not supported netbox version {status}")
+    raise Exception(f"Unsupported version: {status.netbox_version}")
 
 
 class NetboxProvider(StorageProvider, AdapterWithName, AdapterWithConfig):
