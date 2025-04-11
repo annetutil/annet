d8c7a00501429f60b6b16bbaa83ac464e8bfa7ca
Alexey Alekseev <bojika@gmail.com>
2025-04-11 11:42:46 +0300

    Fix testes on Windows platform

--- tests/annet/test_fs_storage.py
+++ tests/annet/test_fs_storage.py
@@ -2,11 +2,22 @@ from annet.adapters.file.provider import FS, Query, StorageOpts, Device
 from annet.storage import StorageProvider, Storage
 import typing
 import tempfile
+import sys
+import platform
+
+kwargs = dict()
+if platform.system() == "Windows":
+    if sys.version_info < (3, 12):
+        kwargs = {"delete": False}
+    else:
+        kwargs = {"delete": True, "delete_on_close": False}
+
 
 def test_fs():
     Device
-    with tempfile.NamedTemporaryFile(delete=True, delete_on_close=False) as f:
-        f.write(b"""
+    with tempfile.NamedTemporaryFile(**kwargs) as f:
+        f.write(
+            b"""
 devices:
   - hostname: hostname
     fqdn: hostname.domain
@@ -14,7 +25,8 @@ devices:
     interfaces:
       - name: eth0
         description: description
-""")
+"""
+        )
         f.flush()
         fs = FS(StorageOpts(path=f.name))
     print(fs)
