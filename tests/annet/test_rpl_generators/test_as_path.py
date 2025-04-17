e2cc0916e807565f0e83827704136fff1198d9d0
Andrey Tikhonov <17@itishka.org>
2025-04-17 17:44:45 +0200

    Test iosxr as-path-filters

--- tests/annet/test_rpl_generators/test_as_path.py
+++ tests/annet/test_rpl_generators/test_as_path.py
@@ -1,15 +1,18 @@
-from typing import Any
+from typing import Any, Sequence
 from unittest.mock import Mock
 
+from annet.annlib.tabparser import parse_to_tree
 from annet.rpl import R, RouteMap, Route, RoutingPolicy
 from annet.rpl_generators import (
-    IpPrefixList, CumulusPolicyGenerator
+    IpPrefixList, CumulusPolicyGenerator, RoutingPolicyGenerator, RDFilter, CommunityList, AsPathFilterGenerator,
+    AsPathFilter
 )
-from .helpers import scrub, cumulus
+from annet.tabparser import make_formatter
+from .helpers import scrub, cumulus, iosxr
 from .. import MockDevice
 
 
-def gen(routemaps: RouteMap, dev: MockDevice) -> str:
+def gen(routemaps: RouteMap, as_path_filters: list[AsPathFilter], dev: MockDevice) -> str:
     class TestCumulusPolicyGenerator(CumulusPolicyGenerator):
         def get_policies(self, device: Any) -> list[RoutingPolicy]:
             return routemaps.apply(device)
@@ -21,7 +24,15 @@ def gen(routemaps: RouteMap, dev: MockDevice) -> str:
             return []
 
         def get_as_path_filters(self, _: Any) -> list:
-            return []
+            return as_path_filters
+
+    class BlackboxAsPathGenerator(AsPathFilterGenerator):
+
+        def get_policies(self, device: Any) -> list[RoutingPolicy]:
+            return routemaps.apply(device)
+
+        def get_as_path_filters(self, device: Any) -> Sequence[AsPathFilter]:
+            return as_path_filters
 
     if dev.hw.soft.startswith("Cumulus"):
         generator = TestCumulusPolicyGenerator()
@@ -29,7 +40,13 @@ def gen(routemaps: RouteMap, dev: MockDevice) -> str:
         result = [" ".join(x) for x in genoutput]
         text = "\n".join(result)
     else:
-        raise ValueError("Unsupported device")
+        result: list[str] = []
+        storage = Mock()
+        generator = BlackboxAsPathGenerator(storage)
+        result.append(generator(dev))
+        fmtr = make_formatter(dev.hw)
+        tree = parse_to_tree("\n".join(result), fmtr.split)
+        text = fmtr.join(tree)
     return scrub(text)
 
 
@@ -47,7 +64,7 @@ def test_cumulus_as_path_set():
         with route(name="n40", number=40) as rule:
             rule.as_path.expand_last_as("65434")
 
-    result = gen(routemaps, cumulus())
+    result = gen(routemaps, [], cumulus())
     expected = scrub("""
 !
 route-map policy permit 10
@@ -67,5 +84,31 @@ route-map policy permit 40
   set as-path prepend last-as 65434
   on-match next
 !
+""")
+    assert result == expected
+
+
+def test_iosxr_as_path_fitlers():
+    routemaps = RouteMap[Mock]()
+    aspath_filters = [
+        AsPathFilter("asf1", ["123", "456"]),
+        AsPathFilter("asf2", ["123"]),
+        AsPathFilter("asf3", ["111"]),
+    ]
+
+    @routemaps
+    def policy(device: Mock, route: Route):
+        with route(R.as_path_filter("asf1"), name="n10", number=10) as rule:
+            rule.allow()
+        with route(R.as_path_filter("asf2"), name="n20", number=20) as rule:
+            rule.allow()
+
+    result = gen(routemaps, aspath_filters, iosxr())
+    expected = scrub("""
+as-path-set asf1
+  ios-regex '123',
+  ios-regex '456'
+as-path-set asf2
+  ios-regex '123'
 """)
     assert result == expected
