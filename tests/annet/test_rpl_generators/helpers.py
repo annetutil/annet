a390c5e9a161cb27c0108521a7470586e694e5fd
Andrey Tikhonov <17@itishka.org>
2025-04-22 18:12:56 +0200

    unify generator helpers

--- tests/annet/test_rpl_generators/helpers.py
+++ tests/annet/test_rpl_generators/helpers.py
@@ -1,3 +1,14 @@
+from collections.abc import Iterator
+from typing import Any, Sequence
+from unittest.mock import Mock
+
+from annet.annlib.tabparser import parse_to_tree
+from annet.rpl import R, RouteMap, Route, RoutingPolicy
+from annet.rpl_generators import (
+    IpPrefixList, CumulusPolicyGenerator, RoutingPolicyGenerator, RDFilter, CommunityList, AsPathFilterGenerator,
+    AsPathFilter,  RDFilterFilterGenerator, PrefixListFilterGenerator, CommunityListGenerator
+)
+from annet.tabparser import make_formatter
 from .. import MockDevice
 
 
@@ -35,3 +46,121 @@ def iosxr():
         "Cisco IOS XR Release 7.4.2",
         "iosxr",
     )
+
+
+def cumulus_generator(
+    routemaps: RouteMap,
+    as_path_filters: list[AsPathFilter] | None = None,
+    community_lists: list[CommunityList] | None = None,
+    prefix_lists: list[IpPrefixList] | None = None,
+):
+    class TestCumulusPolicyGenerator(CumulusPolicyGenerator):
+        def get_policies(self, device: Any) -> list[RoutingPolicy]:
+            return routemaps.apply(device)
+
+        def get_prefix_lists(self, device: Any) -> list[IpPrefixList]:
+            return prefix_lists or []
+
+        def get_community_lists(self, _: Any) -> list:
+            return community_lists or []
+
+        def get_as_path_filters(self, _: Any) -> list:
+            return as_path_filters or []
+
+        def __call__(self, device: Any) -> Iterator[Sequence[str]]:
+            return self.generate_cumulus_rpl(device)
+
+    return TestCumulusPolicyGenerator()
+
+
+def blackbox_generators(
+    routemaps: RouteMap,
+    as_path_filters: list[AsPathFilter] | None = None,
+    community_lists: list[CommunityList] | None = None,
+    prefix_lists: list[IpPrefixList] | None = None,
+    rd_filters: list[RDFilter] | None = None,
+):
+    class BlackboxRdFilterGenerator(RDFilterFilterGenerator):
+        def get_policies(self, device: Any) -> list[RoutingPolicy]:
+            return routemaps.apply(device)
+
+        def get_rd_filters(self, device: Any) -> list[RDFilter]:
+            return rd_filters or []
+
+    class BlackboxPrefixListFilterGenerator(PrefixListFilterGenerator):
+        def get_policies(self, device: Any) -> list[RoutingPolicy]:
+            return routemaps.apply(device)
+
+        def get_prefix_lists(self, device: Any) -> Sequence[IpPrefixList]:
+            return prefix_lists or []
+
+    class BlackboxAsPathGenerator(AsPathFilterGenerator):
+        def get_policies(self, device: Any) -> list[RoutingPolicy]:
+            return routemaps.apply(device)
+
+        def get_as_path_filters(self, device: Any) -> Sequence[AsPathFilter]:
+            return as_path_filters or []
+
+    class BlackboxCommunityListGenerator(CommunityListGenerator):
+        def get_policies(self, device: Any) -> list[RoutingPolicy]:
+            return routemaps.apply(device)
+
+        def get_community_lists(self, device: Any) -> Sequence[CommunityList]:
+            return community_lists or []
+
+    class BlackBoxPolicyGenerator(RoutingPolicyGenerator):
+        def get_prefix_lists(self, device: Any) -> list[IpPrefixList]:
+            return prefix_lists or []
+
+        def get_policies(self, device: Any) -> list[RoutingPolicy]:
+            return routemaps.apply(device)
+
+        def get_community_lists(self, device: Any) -> list[CommunityList]:
+            return community_lists or []
+
+        def get_rd_filters(self, device: Any) -> list[RDFilter]:
+            return rd_filters or []
+
+    storage = Mock()
+    return [
+        BlackboxPrefixListFilterGenerator(storage),
+        BlackboxRdFilterGenerator(storage),
+        BlackboxAsPathGenerator(storage),
+        BlackboxCommunityListGenerator(storage),
+        BlackBoxPolicyGenerator(storage),
+    ]
+
+
+def generate(
+    dev: MockDevice,
+    routemaps: RouteMap,
+    as_path_filters: list[AsPathFilter] | None = None,
+    community_lists: list[CommunityList] | None = None,
+    prefix_lists: list[IpPrefixList] | None = None,
+    rd_filters: list[RDFilter] | None = None,
+):
+    if dev.hw.soft.startswith("Cumulus"):
+        generator = cumulus_generator(
+            routemaps=routemaps,
+            as_path_filters=as_path_filters,
+            community_lists=community_lists,
+            prefix_lists=prefix_lists,
+        )
+        genoutput = generator.generate_cumulus_rpl(dev)
+        result = [" ".join(x) for x in genoutput]
+        return "\n".join(result)
+
+    generators = blackbox_generators(
+        routemaps=routemaps,
+        as_path_filters=as_path_filters,
+        community_lists=community_lists,
+        prefix_lists=prefix_lists,
+        rd_filters=rd_filters,
+    )
+    result: list[str] = []
+    for generator in generators:
+        if generator.supports_device(dev):
+            result.append(generator(dev))
+    fmtr = make_formatter(dev.hw)
+    tree = parse_to_tree("\n".join(result), fmtr.split)
+    return fmtr.join(tree)
--- tests/annet/test_rpl_generators/test_as_path.py
+++ tests/annet/test_rpl_generators/test_as_path.py
@@ -1,67 +1,10 @@
-from typing import Any, Sequence
 from unittest.mock import Mock
 
-from annet.annlib.tabparser import parse_to_tree
-from annet.rpl import R, RouteMap, Route, RoutingPolicy
+from annet.rpl import R, RouteMap, Route
 from annet.rpl_generators import (
-    IpPrefixList, CumulusPolicyGenerator, RoutingPolicyGenerator, RDFilter, CommunityList, AsPathFilterGenerator,
     AsPathFilter
 )
-from annet.tabparser import make_formatter
-from .helpers import scrub, cumulus, iosxr
-from .. import MockDevice
-
-
-def gen(routemaps: RouteMap, as_path_filters: list[AsPathFilter], dev: MockDevice) -> str:
-    class TestCumulusPolicyGenerator(CumulusPolicyGenerator):
-        def get_policies(self, device: Any) -> list[RoutingPolicy]:
-            return routemaps.apply(device)
-
-        def get_prefix_lists(self, device: Any) -> list[IpPrefixList]:
-            return []
-
-        def get_community_lists(self, _: Any) -> list:
-            return []
-
-        def get_as_path_filters(self, _: Any) -> list:
-            return as_path_filters
-
-    class BlackboxAsPathGenerator(AsPathFilterGenerator):
-        def get_policies(self, device: Any) -> list[RoutingPolicy]:
-            return routemaps.apply(device)
-
-        def get_as_path_filters(self, device: Any) -> Sequence[AsPathFilter]:
-            return as_path_filters
-
-    class BlackBoxPolicyGenerator(RoutingPolicyGenerator):
-        def get_prefix_lists(self, device: Any) -> list[IpPrefixList]:
-            return []
-
-        def get_policies(self, device: Any) -> list[RoutingPolicy]:
-            return routemaps.apply(device)
-
-        def get_community_lists(self, device: Any) -> list[CommunityList]:
-            return []
-
-        def get_rd_filters(self, device: Any) -> list[RDFilter]:
-            return []
-
-    if dev.hw.soft.startswith("Cumulus"):
-        generator = TestCumulusPolicyGenerator()
-        genoutput = generator.generate_cumulus_rpl(dev)
-        result = [" ".join(x) for x in genoutput]
-        text = "\n".join(result)
-    else:
-        result: list[str] = []
-        storage = Mock()
-        generator = BlackboxAsPathGenerator(storage)
-        result.append(generator(dev))
-        generator = BlackBoxPolicyGenerator(storage)
-        result.append(generator(dev))
-        fmtr = make_formatter(dev.hw)
-        tree = parse_to_tree("\n".join(result), fmtr.split)
-        text = fmtr.join(tree)
-    return scrub(text)
+from .helpers import scrub, cumulus, iosxr, generate
 
 
 def test_cumulus_as_path_set():
@@ -78,7 +21,7 @@ def test_cumulus_as_path_set():
         with route(name="n40", number=40) as rule:
             rule.as_path.expand_last_as("65434")
 
-    result = gen(routemaps, [], cumulus())
+    result = generate(routemaps=routemaps, dev=cumulus())
     expected = scrub("""
 !
 route-map policy permit 10
@@ -117,7 +60,7 @@ def test_iosxr_as_path_fitlers():
         with route(R.as_path_filter("asf2"), name="n20", number=20) as rule:
             rule.allow()
 
-    result = gen(routemaps, aspath_filters, iosxr())
+    result = generate(routemaps=routemaps, as_path_filters=aspath_filters, dev=iosxr())
     expected = scrub("""
 as-path-set asf1
   ios-regex '123',
@@ -140,7 +83,7 @@ def test_iosxr_as_path_change():
         with route(name="n20", number=20) as rule:
             rule.as_path.prepend("65432")
 
-    result = gen(routemaps, [], iosxr())
+    result = generate(routemaps=routemaps, dev=iosxr())
     expected = scrub("""
 route-policy policy
   prepend as-path 65432
