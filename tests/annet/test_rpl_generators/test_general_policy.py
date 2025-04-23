761a63b0a17c516d70236508e515c63a0cbfcaf5
Andrey Tikhonov <17@itishka.org>
2025-04-23 17:32:51 +0200

    Test then actions

--- tests/annet/test_rpl_generators/test_general_policy.py
+++ tests/annet/test_rpl_generators/test_general_policy.py
@@ -104,3 +104,75 @@ route-policy policy1
     done
 """)
     assert result == expected
+
+
+def test_iosxr_then():
+    routemaps = RouteMap[Mock]()
+    @routemaps
+    def policy1(device: Mock, route: Route):
+        with route(R.as_path_length==1) as rule:
+            rule.set_metric(1)
+        with route(R.as_path_length==2) as rule:
+            rule.add_metric(2)
+        with route(R.as_path_length==3) as rule:
+            rule.set_tag(100)
+        with route(R.as_path_length==4) as rule:
+            rule.next_hop.discard()
+        with route(R.as_path_length==5) as rule:
+            rule.next_hop.self()
+        with route(R.as_path_length==6) as rule:
+            rule.next_hop.peer()
+        with route(R.as_path_length==7) as rule:
+            rule.next_hop.ipv4_addr("192.168.1.1")
+        with route(R.as_path_length==8) as rule:
+            rule.next_hop.ipv6_addr("FE80::1")
+        with route(R.as_path_length==9) as rule:
+            rule.next_hop.mapped_ipv4("192.168.1.1")
+        with route(R.as_path_length==10) as rule:
+            rule.set_metric_type("type-1")
+        with route(R.as_path_length==11) as rule:
+            rule.set_origin("egp")
+        with route(R.as_path_length==12) as rule:
+            rule.set_local_pref(42)
+
+
+    result = generate(routemaps=routemaps, dev=iosxr())
+    expected = scrub("""
+route-policy policy1
+  if as-path length eq 1 then
+    set med 1
+    pass
+  if as-path length eq 2 then
+    set med +2
+    pass
+  if as-path length eq 3 then
+    set tag 100
+    pass
+  if as-path length eq 4 then
+    set next-hop discard
+    pass
+  if as-path length eq 5 then
+    set next-hop self
+    pass
+  if as-path length eq 6 then
+    pass
+  if as-path length eq 7 then
+    set next-hop 192.168.1.1
+    pass
+  if as-path length eq 8 then
+    set next-hop FE80::1
+    pass
+  if as-path length eq 9 then
+    set next-hop ::FFFF:192.168.1.1
+    pass
+  if as-path length eq 10 then
+    set metric-type type-1
+    pass
+  if as-path length eq 11 then
+    set origin egp
+    pass
+  if as-path length eq 12 then
+    set local-preference 42
+    pass
+""")
+    assert result == expected
