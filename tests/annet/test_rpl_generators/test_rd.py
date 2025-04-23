2ef3d473bda76dae2a371b05069a6e892f75d480
Andrey Tikhonov <17@itishka.org>
2025-04-23 16:27:20 +0200

    test rd

---
+++ tests/annet/test_rpl_generators/test_rd.py
@@ -0,0 +1,39 @@
+from unittest.mock import Mock
+
+from annet.rpl import R, RouteMap, Route
+from annet.rpl_generators import RDFilter
+from .helpers import scrub, cumulus, iosxr, generate
+
+
+def test_iosxr_as_path_change():
+    routemaps = RouteMap[Mock]()
+
+    rd_filters = [
+        RDFilter("rd1", 1, ["172.16.0.0/16:*", "172.16.0.0/16:100"]),
+        RDFilter("rd2", 2, ["192:*", "192:100"]),
+    ]
+
+    @routemaps
+    def policy(device: Mock, route: Route):
+        with route(R.rd.has("rd1", "rd2"), name="n20", number=20) as rule:
+            rule.allow()
+        with route(R.rd.has_any("rd1", "rd2"), name="n20", number=20) as rule:
+            rule.allow()
+
+    result = generate(routemaps=routemaps, rd_filters=rd_filters, dev=iosxr())
+    expected = scrub("""
+rd-set rd1
+  172.16.0.0/16:*,
+  172.16.0.0/16:100
+rd-set rd2
+  192:*,
+  192:100
+
+route-policy policy
+  if rd in rd1 and rd in rd2 then
+    done
+  if (rd in rd1 or rd in rd2) then
+    done
+
+""")
+    assert result == expected
