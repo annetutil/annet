05b368f1a1008caaf6c59254ceb3bc5852f4317b
Alexey Alekseev <bojika@gmail.com>
2025-04-24 17:57:53 +0300

    fix cisco banner

--- annet/annlib/tabparser.py
+++ annet/annlib/tabparser.py
@@ -314,6 +314,13 @@ class CiscoFormatter(BlockExitFormatter):
     def split(self, text):
         additional_indent = 0
         block_exit_strings = [self._block_exit]
+        banner = re.compile(r"((^banner [a-z-]+) \^C.*?\^C)", flags=re.MULTILINE | re.DOTALL)
+        m = banner.findall(text)
+        repl_map = {replace_str:banner_str for banner_str, replace_str in m}
+
+        for replace_str, banner in repl_map.items():
+            text = text.replace(banner, replace_str)
+
         tree = self.split_remove_spaces(text)
         for i, item in enumerate(tree):
             block_exit_strings, new_indent = self._split_indent(
@@ -321,6 +328,12 @@ class CiscoFormatter(BlockExitFormatter):
             )
             tree[i] = f"{' ' * additional_indent}{item}"
             additional_indent = new_indent
+
+
+        for i, item in enumerate(tree):
+            if item in repl_map:
+                tree[i] = repl_map[item]
+
         return tree
 
     def block_exit(self, context: Optional[FormatterContext]) -> str:
