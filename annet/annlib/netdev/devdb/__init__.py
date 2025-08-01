814cd602f26aadaca5a56f0df9652e8f6fd24c72
vdjdance <vdjdance@gmail.com>
2025-08-01 14:48:19 +0300

    changed on get_context

--- annet/annlib/netdev/devdb/__init__.py
+++ annet/annlib/netdev/devdb/__init__.py
@@ -6,6 +6,7 @@ from pathlib import Path
 from typing import Any, Dict
 
 from annet.annlib.netdev.db import find_true_sequences, get_db
+from annet.lib import get_context
 
 
 @functools.lru_cache(None)
@@ -24,7 +25,7 @@ def _prepare_db() -> Dict[str, Any]:
         from library.python import resource
         raw = json.loads(resource.resfs_read("contrib/python/annet/annet/annlib/netdev/devdb/data/devdb.json").decode("utf-8"))
     except ImportError:
-        devdb_file = Path(os.environ.get("ANN_DEVDB_JSON_PATH", Path(__file__).parent / "data" / "devdb.json"))
+        devdb_file = Path(get_context().get("devdb", {}).get("path", Path(__file__).parent / "data" / "devdb.json"))
         with devdb_file.open("r", encoding="utf-8") as f:
             raw = json.load(f)
     return {tuple(seq.split(".")): re.compile(regexp) for (seq, regexp) in raw.items()}
