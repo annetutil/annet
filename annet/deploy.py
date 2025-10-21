6d1fd19ec08bb625637e0f36ac655d7e993ab099
Ivan Kruchinin <vafielb@gmail.com>
2025-10-21 15:47:48 +0300

    add: default read_timeout to cmd_timeout value

--- annet/deploy.py
+++ annet/deploy.py
@@ -195,6 +195,8 @@ def fill_cmd_params(rules: OrderedDict, cmd: Command):
         cmd.questions = cmd_params.get("questions", None)
         if cmd.timeout is None:
             cmd.timeout = cmd_params["timeout"]
+        if cmd.read_timeout is None:
+            cmd.read_timeout = cmd.timeout
 
 
 def apply_deploy_rulebook(hw: HardwareView, cmd_paths, do_finalize=True, do_commit=True):
