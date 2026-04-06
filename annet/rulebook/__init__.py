a1acd4d2daa8cd5b3d5ba924403d852879157dca
Aleksandr Raspopov <147997333+Dosto4k@users.noreply.github.com>
2026-04-06 20:36:49 +0300

    feat: add feature to pass custom path to rulebook module (#510)
    Co-authored-by: Aleksandr Raspopov <dosto4k@yandex-team.ru>

--- annet/rulebook/__init__.py
+++ annet/rulebook/__init__.py
@@ -25,15 +25,16 @@ class Rulebook(TypedDict):
     texts: RulebookTexts
 
 
-def get_rulebook(hw) -> Rulebook:
-    return RulebookProvider().get_rulebook(hw)
+def get_rulebook(hw, rulebook_module: str | None = None) -> Rulebook:
+    return RulebookProvider(rulebook_module).get_rulebook(hw)
 
 
 class RulebookProvider:
-    def __init__(self):
+    def __init__(self, rulebook_module: str | None = None):
         self._rulebook_cache = {}
         self._render_rul_cache = {}
         self._escaped_rul_cache = {}
+        self.rulebook_module = self._get_rulebook_module(rulebook_module)
 
     def get_rulebook(self, hw) -> Rulebook:
         if hw in self._rulebook_cache:
@@ -70,6 +71,20 @@ class RulebookProvider:
         }
         return self._rulebook_cache[hw]
 
+    def _get_rulebook_module(self, rulebook_module: str | None = None) -> str:
+        if rulebook_module is not None:
+            path_to_module = rulebook_module
+            module = import_module(path_to_module)
+        else:
+            try:
+                path_to_module = get_context()["rulebooks"]["path"]
+            except KeyError:
+                raise KeyError("No path to rulebooks in the context.yml")
+            module = import_module(path_to_module)
+        if module.__file__ is None:
+            raise ModuleNotFoundError(f"File __init__.py missing from the {path_to_module} module.")
+        return module
+
     def _render_rul(self, name, hw):
         key = (name, hw)
         if key not in self._render_rul_cache:
@@ -79,17 +94,9 @@ class RulebookProvider:
     def _read_escaped_rul(self, name):
         if name in self._escaped_rul_cache:
             return self._escaped_rul_cache[name]
-        context = get_context()
+        path_to_rulebook = os.path.dirname(self.rulebook_module.__file__)
         try:
-            python_path_to_module = context["rulebooks"]["path"]
-        except KeyError:
-            raise KeyError("No path to rulebooks in the context.yml")
-        module = import_module(python_path_to_module)
-        if module.__file__ is None:
-            raise ModuleNotFoundError(f"File __init__.py missing from the {python_path_to_module} module.")
-        path_to_rulebook_dir = os.path.dirname(module.__file__)
-        try:
-            with open(os.path.join(path_to_rulebook_dir, name), "r", encoding="utf-8") as f:
+            with open(os.path.join(path_to_rulebook, name), "r", encoding="utf-8") as f:
                 self._escaped_rul_cache[name] = self._escape_mako(f.read())
                 return self._escaped_rul_cache[name]
         except FileNotFoundError:
