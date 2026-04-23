30fc421fc6dae2fc70c9433d07740861761eb351
Denis Nabokov <denisnabokov@icloud.com>
2026-04-23 17:19:18 +0300

    add b4com syslog rulebook

--- annet/rulebook/b4com/misc.py
+++ annet/rulebook/b4com/misc.py
@@ -7,3 +7,16 @@ def undo_username(rule, key, diff, **_):
         yield (False, f"no username {key[0]}", None)
     else:
         yield from common.default(rule, key, diff)
+
+
+def undo_syslog(rule, key, diff, **kwargs):
+    if diff[Op.REMOVED]:
+        parts = key[1].split()
+        ip = parts[0]
+        vrf = parts[-1]
+        if "vrf" in key[1]:
+            yield (False, f"no logging remote server {ip} vrf {vrf}", None)
+        else:
+            yield (False, f"no logging remote server {ip}", None)
+    else:
+        yield from common.default(rule, key, diff, **kwargs)
