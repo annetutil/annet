bb55ed62ee43ccffd9dd47884b6f0bc7b923d38b
Aleksandr Raspopov <147997333+Dosto4k@users.noreply.github.com>
2026-05-12 19:56:25 +0300

    fix: allow duplicate rules with different scope (#545)
    * fix: allow duplicate rules with different scope
    
    * test: add invalid scope cases
    
    * refactor: fix typo and refactor check scope compatibility func
    
    ---------
    
    Co-authored-by: Aleksandr Raspopov <dosto4k@yandex-team.ru>

--- tests/annet/test_inherit/conftest.py
+++ tests/annet/test_inherit/conftest.py
@@ -9,10 +9,11 @@ from tests.annet.test_inherit import TestRulebookNotFoundError, mock_get_raw_rul
 def mock_rulebooks(request):
     """Fixture for mock rulebooks"""
     file, test_case, rulebooks = request.param
+    error_message = rulebooks.pop("error_message", None)
     mock_obj = mock_get_raw_rulebook_text(rulebooks)
 
     with mock.patch("annet.rulebook.DefaultRulebookProvider._get_raw_rulebook_text", side_effect=mock_obj):
         try:
-            yield {"file": file, "test_case": test_case}
+            yield {"file": file, "test_case": test_case, "error_message": error_message}
         except TestRulebookNotFoundError as err:
             raise TestRulebookNotFoundError(f"Rulebook not found in file '{file}', test case '{test_case}'.") from err
---
+++ tests/annet/test_inherit/test_ordering/test_invalid_cases.py
@@ -0,0 +1,43 @@
+import re
+
+import pytest
+
+from annet.rulebook import DefaultRulebookProvider
+from annet.rulebook.exceptions import RulebookSyntaxError
+from tests import make_hw_stub
+from tests.annet.test_inherit import IncorrectErrorMessage, get_tests_data
+
+
+@pytest.mark.parametrize(
+    "mock_rulebooks",
+    get_tests_data("annet/test_inherit/test_ordering/test_invalid_cases"),
+    ids=lambda val: f"file={val[0]}, case={val[1]}",
+    indirect=True,
+)
+def test_invalid_cases(mock_rulebooks):
+    rb_provider = DefaultRulebookProvider()
+    hw = make_hw_stub("juniper")
+    vendor = hw.vendor
+
+    if mock_rulebooks["error_message"] is None:
+        raise IncorrectErrorMessage(
+            f"The error_message field in file {mock_rulebooks['file']} "
+            f"in test case {mock_rulebooks['test_case']} is not defined."
+        )
+    if not isinstance(mock_rulebooks["error_message"], str):
+        raise IncorrectErrorMessage(
+            f"The error_message field in file {mock_rulebooks['file']} "
+            f"in test case {mock_rulebooks['test_case']} is not a string."
+        )
+
+    with pytest.raises(RulebookSyntaxError) as exc_info:
+        rb_provider._get_rulebook_by_extension(rulebook_path="child", extension="order", hw=hw, vendor=vendor)
+
+    expected_pattern_raw = mock_rulebooks["error_message"]
+    expected_pattern_escaped = re.escape(expected_pattern_raw)
+    actual_error_message = str(exc_info.value)
+
+    assert re.search(expected_pattern_escaped, actual_error_message) is not None, (
+        f'The received error message "{actual_error_message}" does not '
+        f'contain the expected error_message "{expected_pattern_raw}".'
+    )
