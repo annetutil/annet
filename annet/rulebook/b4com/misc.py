8a78f3ca617052f0d9e72e3a029659da7c6f788e
Роман Карауланов <anteron@mts.ru>
2026-06-09 12:48:40 +0300

    Linters fixes

--- annet/rulebook/b4com/misc.py
+++ annet/rulebook/b4com/misc.py
@@ -11,7 +11,7 @@ def undo_username(rule, key, diff, **kwargs):
         else:
             removed_diff = diff[Op.REMOVED][0]["row"]
             if "sshkey" in removed_diff:
-                yield (False, f"no username {key[0]} sshkey {removed_diff.split("ssh-rsa")[-1]}", None)
+                yield (False, f"no username {key[0]} sshkey {removed_diff.split('ssh-rsa')[-1]}", None)
                 return
             yield from common.default(rule, key, diff)
     else:
