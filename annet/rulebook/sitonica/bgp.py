95b9877044430a3f920a0353daadcd2e07bc848e
karajomok <karajomok@gmail.com>
2026-06-17 22:53:33 +0300

    fix: rulebooks

--- annet/rulebook/sitonica/bgp.py
+++ annet/rulebook/sitonica/bgp.py
@@ -14,15 +14,27 @@ def is_ipaddress(address: str) -> bool:
 
 def undo_peer(rule, key, diff, **kwargs):
     if diff[Op.REMOVED]:
+        # if "description" in diff[Op.REMOVED][0]["row"]:
+        #     import ipdb; ipdb.set_trace()
         neighbor = diff[Op.REMOVED][0]["row"].split()[1]
-        is_neighbor_removing = False
+        is_peer_deleted = False
+        is_peer_disabled = False
         for item in kwargs["rule_pre"]["items"].values():
             if item[Op.REMOVED]:
-                if "as-number" in item[Op.REMOVED][0]["row"]:
-                    is_neighbor_removing = True
+                if " as-number " in item[Op.REMOVED][0]["row"]:
+                    is_peer_deleted = True
+                    break
+                if " group " in item[Op.REMOVED][0]["row"]:
+                    is_peer_deleted = True
+                    break
+                if " enable" in item[Op.REMOVED][0]["row"]:
+                    is_peer_disabled = True
+                    break
 
-        if is_neighbor_removing:
+        if is_peer_deleted:
             yield (False, f"undo peer {neighbor}", None)
+        elif is_peer_disabled:
+            yield (False, f"undo peer {neighbor} enable", None)
         else:
             yield from common.default(rule, key, diff, **kwargs)
     else:
