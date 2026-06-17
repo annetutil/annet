aa4d56fdb306bab61c592472019178646c17f1f4
karajomok <karajomok@gmail.com>
2026-06-17 23:28:09 +0300

    feat: add rulebooks and tests for all kind of BGP configurations

--- annet/rulebook/sitonica/bgp.py
+++ annet/rulebook/sitonica/bgp.py
@@ -14,28 +14,35 @@ def is_ipaddress(address: str) -> bool:
 
 def undo_peer(rule, key, diff, **kwargs):
     if diff[Op.REMOVED]:
-        # if "description" in diff[Op.REMOVED][0]["row"]:
-        #     import ipdb; ipdb.set_trace()
-        neighbor = diff[Op.REMOVED][0]["row"].split()[1]
+        peer = diff[Op.REMOVED][0]["row"].split()[1]
         is_peer_deleted = False
         is_peer_disabled = False
         for item in kwargs["rule_pre"]["items"].values():
             if item[Op.REMOVED]:
-                if " as-number " in item[Op.REMOVED][0]["row"]:
-                    is_peer_deleted = True
-                    break
-                if " group " in item[Op.REMOVED][0]["row"]:
-                    is_peer_deleted = True
-                    break
+                if is_ipaddress(peer):
+                    if " as-number " in item[Op.REMOVED][0]["row"]:
+                        is_peer_deleted = True
+                        break
+                    if " group " in item[Op.REMOVED][0]["row"]:
+                        is_peer_deleted = True
+                        break
+                else:
+                    if "group " in item[Op.REMOVED][0]["row"]:
+                        is_peer_deleted = True
+                        break
                 if " enable" in item[Op.REMOVED][0]["row"]:
                     is_peer_disabled = True
                     break
 
         if is_peer_deleted:
-            yield (False, f"undo peer {neighbor}", None)
+            if is_ipaddress(peer):
+                yield (False, f"undo peer {peer}", None)
+            else:
+                yield (False, f"undo group {peer}", None)
         elif is_peer_disabled:
-            yield (False, f"undo peer {neighbor} enable", None)
+            yield (False, f"undo peer {peer} enable", None)
         else:
             yield from common.default(rule, key, diff, **kwargs)
+
     else:
         yield from common.default(rule, key, diff, **kwargs)
